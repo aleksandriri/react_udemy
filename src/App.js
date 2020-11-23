95ec2a1c904a5f0824574ac13978cc4b080eafa7
Aleksandr Heletukha <heletukha@gmail.com>
2020-11-23 16:37:26 +0200

    Add map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Car from "./car/car";
 import state from "./State/state";
 
+
 let App = (props) => {
     return (
         <div className="App" style={{textAlign: 'left'}}>
             <h1>Lorem{state.pageTitle}</h1>
-            <Car name={state.cars[0].name} model={state.cars[0].model} year={state.cars[0].year} description={state.cars[0].description}/>
-            <Car name={state.cars[1].name} year={state.cars[1].year}/>
-            <Car name={state.cars[2].name} year={state.cars[2].year}/>
+            {
+                state.cars.map((car) => {
+                    return (
+                        <Car
+                            name={car.name}
+                            year={car.year}
+                            description={car.description}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
